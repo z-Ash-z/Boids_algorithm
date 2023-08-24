d4d3f4d5b833f2d768041892cc11898498637569
Aneesh Chodisetty <85113634+z-Ash-z@users.noreply.github.com>
2023-08-23 20:24:41 -0400

    Optimizing using threads

--- Runner.py
+++ Runner.py
@@ -1,3 +1,6 @@
+import time
+import threading
+
 from Utils import Boids
 from Utils import Stage
 
@@ -9,12 +12,17 @@ def main() -> None:
     for _ in range(100):
         boids.append(Boids(stage.WIDTH, stage.HEIGHT))
 
-    while True:
+    start_time = time.time()
+
+    for _ in range(1_000):
         for boid in boids:
             boid.flock(boids)
             boid.update()
             stage.drawBoid(boid)
         stage.show("Boids Algorithm")
+
+    end_time = time.time()
+    print(f'The time it took for 1000 iterations is: {end_time-start_time}sec')
 
 if __name__ == '__main__':
     main()
