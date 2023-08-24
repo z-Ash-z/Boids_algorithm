0a4458416788557048157bba924a126a93b3128a
Aneesh Chodisetty <85113634+z-Ash-z@users.noreply.github.com>
2023-08-23 20:57:12 -0400

    Testing threads

--- Runner.py
+++ Runner.py
@@ -4,6 +4,12 @@ import threading
 from Utils import Boids
 from Utils import Stage
 
+
+def consolidate(boid, boids, stage) -> None:
+    boid.flock(boids)
+    boid.update()
+    stage.drawBoid(boid)
+
 def main() -> None:
     stage = Stage(1000, 1000)
 
@@ -11,17 +17,25 @@ def main() -> None:
 
     for _ in range(100):
         boids.append(Boids(stage.WIDTH, stage.HEIGHT))
-
-    start_time = time.time()
+    
+    start_time = time.perf_counter()
 
     for _ in range(1_000):
+        
+        threads = list()
+
         for boid in boids:
-            boid.flock(boids)
-            boid.update()
-            stage.drawBoid(boid)
+            thread = threading.Thread(target=consolidate, args=(boid, boids, stage))   
+            threads.append(thread)
+
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
         stage.show("Boids Algorithm")
 
-    end_time = time.time()
+    end_time = time.perf_counter()
     print(f'The time it took for 1000 iterations is: {end_time-start_time}sec')
 
 if __name__ == '__main__':
