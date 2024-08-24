dfb5127658b5e6adc2ded683904c2b3d3e4facda
Aneesh Chodisetty <85113634+z-Ash-z@users.noreply.github.com>
2024-08-24 00:31:19 -0400

    Added typying and modified thread behaviour

--- Runner.py
+++ Runner.py
@@ -1,32 +1,36 @@
 import time
 import threading
 import concurrent.futures
+import typing
 
 from Utils import Boids
 from Utils import Stage
 
 
-def consolidate(boid, boids) -> None:
+def consolidate(boid : Boids, boids : typing.List[Boids], stage : Stage) -> None:
     boid.flock(boids)
     boid.update()
-    # stage.drawBoid(boid)
+    stage.drawBoid(boid)
 
 def main() -> None:
     stage = Stage(1000, 1000)
 
     boids : list[Boids] = list()
 
-    for _ in range(100):
+    for _ in range(1_00):
         boids.append(Boids(stage.WIDTH, stage.HEIGHT))
     
     start_time = time.perf_counter()
 
-    for _ in range(1):
+    for _ in range(1_000):
         
-        # threads = list()
+        threads = list()
+
+        positions = Boids.getCopy(boids)
 
         # for boid in boids:
-        #     thread = threading.Thread(target=consolidate, args=(boid, boids, stage))   
+        #     # consolidate(boid, positions, stage)
+        #     thread = threading.Thread(target=consolidate, args=(boid, positions, stage))   
         #     threads.append(thread)
 
         # for thread in threads:
@@ -34,15 +38,20 @@ def main() -> None:
 
         # for thread in threads:
         #     thread.join()
-        with concurrent.futures.ProcessPoolExecutor() as executor:
+        # with concurrent.futures.ProcessPoolExecutor() as executor:
+        #     for boid in boids:
+        #         executor.submit(consolidate, boid, positions, stage)
+        #     executor.shutdown(wait = True)
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
             for boid in boids:
-                executor.submit(consolidate, boid, boids)
-                stage.drawBoid(boid)
+                executor.submit(consolidate, boid, positions, stage)
+            executor.shutdown(wait = True)
 
         stage.show("Boids Algorithm")
 
     end_time = time.perf_counter()
-    print(f'The time it took for 1000 iterations is: {end_time-start_time}sec')
+    print(f'The time it took for 1000 iterations is: {end_time - start_time}sec')
 
 if __name__ == '__main__':
     main()
